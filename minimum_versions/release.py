f39442523f8e10937e15473282ffa2606210af17
Justus Magin <keewis@posteo.de>
2025-09-19 17:53:16 +0200

    add a function to fetch releases

--- minimum_versions/release.py
+++ minimum_versions/release.py
@@ -1,7 +1,10 @@
+import asyncio
 import datetime
 from dataclasses import dataclass, field
 
-from rattler import Version
+from rattler import Gateway, Version
+from tlz.functoolz import curry, pipe
+from tlz.itertoolz import concat, groupby
 
 
 @dataclass(order=True)
@@ -17,3 +20,42 @@ class Release:
             build_number=repo_data.build_number,
             timestamp=repo_data.timestamp,
         )
+
+
+def group_packages(records):
+    groups = groupby(lambda r: r.name.normalized, records)
+    return {
+        name: sorted(map(Release.from_repodata_record, group))
+        for name, group in groups.items()
+    }
+
+
+def filter_releases(predicate, releases):
+    return {
+        name: [r for r in records if predicate(r)] for name, records in releases.items()
+    }
+
+
+def deduplicate_releases(package_info):
+    def deduplicate(releases):
+        return min(releases, key=lambda p: p.timestamp)
+
+    return {
+        name: list(map(deduplicate, groupby(lambda p: p.version, group).values()))
+        for name, group in package_info.items()
+    }
+
+
+def fetch_releases(channels, platforms, all_packages):
+    gateway = Gateway()
+
+    query = gateway.query(channels, platforms, all_packages, recursive=False)
+    records = asyncio.run(query)
+
+    return pipe(
+        records,
+        concat,
+        group_packages,
+        curry(filter_releases, lambda r: r.timestamp is not None),
+        deduplicate_releases,
+    )
